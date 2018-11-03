dd06cac8d043a7e12b80a71128f3bcf110dfa5da
neeil1990 <neeil@mail.ru>
2018-11-03 22:03:36 +0300

    sale.order.ajax confirm order

--- personal/order/make/index.php
+++ personal/order/make/index.php
@@ -1,66 +1,81 @@
 <?
-	require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-	$APPLICATION->SetTitle("Îôîğìëåíèå çàêàçà");
-	global $USER;
-
-
-	$steps = array("Êîíòàêòíûå äàííûå" , "Ïîëó÷åíèå", "Îïëàòà", "Ïîäòâåæäåíèå çàêàçà");
-	
-	if($_GET["step"] == 2)
-		$_SESSION["order_step"] = 2;
-	
-	if(!$_SESSION["order_step"])
-	{
-		if($USER->IsAuthorized())
-			$_SESSION["order_step"] = 2;
-		else
-			$_SESSION["order_step"] = 1;
-	}
-	if($_POST["change_step"])
-		$_SESSION["order_step"] = $_POST["change_step"];
-
-	if(!$USER->IsAuthorized())
-		$_SESSION["order_step"] = 1;
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Îôîğìëåíèå çàêàçà");
 ?>
-	<link rel="stylesheet" type="text/css" href="/personal/order/make/js/style.css"></link>
-	<script src="https://api-maps.yandex.ru/2.1/?lang=ru-RU" type="text/javascript"></script>
-	<script src="/personal/order/make/js/order.js" type="text/javascript"></script>
-	<div class="steps">
-		<?
-			for($i=1;$i<=count($steps);$i++)
-			{
-				?>
-					<div class="step <? if($i == $_SESSION["order_step"]) echo "active"; ?>">
-						<div class="content">
-							<div class="num <? if($i < $_SESSION["order_step"] && $_SESSION["order_step"] > 1) echo 'green'?>">
-							<?
-								if($i < $_SESSION["order_step"] && $_SESSION["order_step"] > 1)
-								{
-									?><img src="/personal/order/make/img/ok.png" /><?
-								}
-								else
-								{
-									?><span><?=$i?></span><?
-								}
-							?>
-							</div>
-							<div class="title">
-							<?
-								if($i < $_SESSION["order_step"])
-								{
-									?>
-										<a class="check-step" data-id="<?=$i?>" href="<?=$_SERVER["REQUEST_URI"]?>"><?=$steps[$i-1]?></a>
-									<?
-								}
-								else
-									echo $steps[$i-1];
-							?>
-							</div>
-						</div>
-					</div>
-				<?
-			}
-		?>
-	</div>
-	<div class="order_container"><?require_once("include/step".$_SESSION["order_step"].".php");?></div>
-	<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+
+<?$APPLICATION->IncludeComponent(
+	"bitrix:sale.order.ajax", 
+	".order.ajax", 
+	array(
+		"ACTION_VARIABLE" => "soa-action",
+		"ADDITIONAL_PICT_PROP_8" => "-",
+		"ALLOW_APPEND_ORDER" => "Y",
+		"ALLOW_AUTO_REGISTER" => "Y",
+		"ALLOW_NEW_PROFILE" => "N",
+		"ALLOW_USER_PROFILES" => "N",
+		"BASKET_IMAGES_SCALING" => "adaptive",
+		"BASKET_POSITION" => "after",
+		"COMPATIBLE_MODE" => "Y",
+		"DELIVERIES_PER_PAGE" => "9",
+		"DELIVERY_FADE_EXTRA_SERVICES" => "N",
+		"DELIVERY_NO_AJAX" => "H",
+		"DELIVERY_NO_SESSION" => "Y",
+		"DELIVERY_TO_PAYSYSTEM" => "d2p",
+		"DISABLE_BASKET_REDIRECT" => "N",
+		"EMPTY_BASKET_HINT_PATH" => "/",
+		"HIDE_ORDER_DESCRIPTION" => "N",
+		"ONLY_FULL_PAY_FROM_ACCOUNT" => "N",
+		"PATH_TO_AUTH" => "/auth/",
+		"PATH_TO_BASKET" => "/personal/cart/",
+		"PATH_TO_PAYMENT" => "payment.php",
+		"PATH_TO_PERSONAL" => "/personal/index.php",
+		"PAY_FROM_ACCOUNT" => "N",
+		"PAY_SYSTEMS_PER_PAGE" => "9",
+		"PICKUPS_PER_PAGE" => "9",
+		"PICKUP_MAP_TYPE" => "yandex",
+		"PRODUCT_COLUMNS_HIDDEN" => array(
+		),
+		"PRODUCT_COLUMNS_VISIBLE" => array(
+			0 => "PREVIEW_PICTURE",
+			1 => "PROPS",
+		),
+		"SEND_NEW_USER_NOTIFY" => "Y",
+		"SERVICES_IMAGES_SCALING" => "adaptive",
+		"SET_TITLE" => "Y",
+		"SHOW_BASKET_HEADERS" => "N",
+		"SHOW_COUPONS_BASKET" => "N",
+		"SHOW_COUPONS_DELIVERY" => "N",
+		"SHOW_COUPONS_PAY_SYSTEM" => "N",
+		"SHOW_DELIVERY_INFO_NAME" => "Y",
+		"SHOW_DELIVERY_LIST_NAMES" => "Y",
+		"SHOW_DELIVERY_PARENT_NAMES" => "Y",
+		"SHOW_MAP_IN_PROPS" => "N",
+		"SHOW_NEAREST_PICKUP" => "N",
+		"SHOW_NOT_CALCULATED_DELIVERIES" => "L",
+		"SHOW_ORDER_BUTTON" => "final_step",
+		"SHOW_PAY_SYSTEM_INFO_NAME" => "Y",
+		"SHOW_PAY_SYSTEM_LIST_NAMES" => "Y",
+		"SHOW_PICKUP_MAP" => "Y",
+		"SHOW_STORES_IMAGES" => "Y",
+		"SHOW_TOTAL_ORDER_BUTTON" => "N",
+		"SHOW_VAT_PRICE" => "N",
+		"SKIP_USELESS_BLOCK" => "N",
+		"SPOT_LOCATION_BY_GEOIP" => "Y",
+		"TEMPLATE_LOCATION" => "popup",
+		"TEMPLATE_THEME" => "site",
+		"USER_CONSENT" => "Y",
+		"USER_CONSENT_ID" => "0",
+		"USER_CONSENT_IS_CHECKED" => "Y",
+		"USER_CONSENT_IS_LOADED" => "N",
+		"USE_CUSTOM_ADDITIONAL_MESSAGES" => "N",
+		"USE_CUSTOM_ERROR_MESSAGES" => "N",
+		"USE_CUSTOM_MAIN_MESSAGES" => "N",
+		"USE_ENHANCED_ECOMMERCE" => "N",
+		"USE_PHONE_NORMALIZATION" => "Y",
+		"USE_PRELOAD" => "Y",
+		"USE_PREPAYMENT" => "N",
+		"USE_YM_GOALS" => "N",
+		"COMPONENT_TEMPLATE" => ".order.ajax"
+	),
+	false
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
