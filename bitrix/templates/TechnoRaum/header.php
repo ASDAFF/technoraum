7e1ff9a6a65744a7baa861e5fffacea8869cadaf
neeil1990 <neeil@mail.ru>
2018-11-19 19:08:30 +0300

    no message

--- bitrix/templates/TechnoRaum/header.php
+++ bitrix/templates/TechnoRaum/header.php
@@ -39,7 +39,28 @@ if($url[1] != "personal" && $url[2] != "order" && $url[3] != "make")
 		<script src="//dcapi.direct-credit.ru/JsHttpRequest.js" type="text/javascript"></script>
 		<script src="//dcapi.direct-credit.ru/dc.js" charset="utf-8" type="text/javascript"></script> 
 
-		<script type="text/javascript"> var partnerID = "9211473"; var debug = true; </script> 
+		<script type="text/javascript">
+			var partnerID = "9211473";
+			var debug = true;
+
+			var arrProducts = new Array();
+			arrProducts[0] = {id : '108', price: '49990', count: '1', type: 'Паропылесосы', name: 'Паропылесос SV 7' };
+
+
+			DCLoans(partnerID, 'delProduct', false, function(result){
+				if (result.status == true) {
+					DCLoans(partnerID, 'addProduct', { products : arrProducts }, function(result){
+						console.log(result);
+						if (result.status == true) {
+							DCLoans(partnerID, 'saveOrder', { order : '123', phone: '9161234567', codeTT: '347344637' }, function(result){
+
+							}, debug);
+						}
+					}, debug);
+				}
+			}, debug);
+
+		</script>
 
 	</head>
 <body>
