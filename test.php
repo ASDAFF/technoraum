2eee73f0047f5408e4e601581af9c2166886cde7
neeil1990 <neeil@mail.ru>
2018-11-20 11:01:04 +0300

    no message

--- test.php
+++ test.php
@@ -3,7 +3,31 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
 ?>
 
+    <script type="text/javascript">
+        var partnerID = "9211473";
 
+        var debug = true;
+
+        var arrProducts = new Array();
+        arrProducts[0] = {id : '108', price: '49990', count: '1', type: 'Паропылесосы', name: 'Паропылесос SV 7' };
+
+        DCLoans(partnerID, 'delProduct', false, function(result){
+            if (result.status == true) {
+                DCLoans(partnerID, 'addProduct', { products : arrProducts }, function(result){
+                    if (result.status == true) {
+                        DCLoans(partnerID, 'saveOrder', {
+                                order : '123',
+                                phone: '9161234567',
+                                codeTT: '347344637',
+                            },
+                            function(result){
+                                console.log(result);
+                            }, debug);
+                    }
+                }, debug);
+            }
+        }, debug);
+    </script>
 
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
