b167e1f60e89c184769c98990b2df0af6529be94
neeil1990 <neeil@mail.ru>
2019-06-12 13:28:05 +0300

    srv

--- bitrix/templates/TechnoRaum/footer.php
+++ bitrix/templates/TechnoRaum/footer.php
@@ -336,6 +336,17 @@ if($_SERVER["REQUEST_URI"] != "/")
             gtag('config', 'UA-125578224-1');
         </script>
         <!-- /Global site tag (gtag.js) - Google Analytics -->
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+(function(){ var widget_id = '3Kdh1V3ZKW';var d=document;var w=window;function l(){
+  var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+  s.src = '//code.jivosite.com/script/widget/'+widget_id
+    ; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}
+  if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}
+  else{w.addEventListener('load',l,false);}}})();
+</script>
+<!-- {/literal} END JIVOSITE CODE -->
+
 
   </body>
 <?
