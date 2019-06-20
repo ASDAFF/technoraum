83f8b842c49d25d72f44e345bb719bbc8dc65219
neeil1990 <neeil@mail.ru>
2019-06-20 20:58:52 +0300

    $APPLICATION->ConvertCharset()

--- ajax/mail-stock-request.php
+++ ajax/mail-stock-request.php
@@ -2,13 +2,14 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 define("STOP_STATISTICS", true);
 CModule::IncludeModule( 'catalog' );
+global $APPLICATION;
 
 if(!$_REQUEST['email'])
     die;
 
 $arFields = array(
     "EMAIL" => $_REQUEST['email'],
-    "TABLE" => iconv("UTF-8","CP1251",trim($_REQUEST['table']))
+    "TABLE" => $APPLICATION->ConvertCharset($_REQUEST['table'], "UTF-8", "windows-1251")
 );
 
 if(CEvent::Send("MAIL_STOCK_LIST", SITE_ID, $arFields)):
